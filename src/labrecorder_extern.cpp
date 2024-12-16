71e6a166c4e36040c115feaac964b8baf65dd761
ericthecurious <hello@ericthecurious.com>
2024-12-15 23:52:38 -0700

    patch: type casts

--- src/labrecorder_extern.cpp
+++ src/labrecorder_extern.cpp
@@ -2,13 +2,19 @@
 #include "../xdfwriter/xdfwriter.h"
 
 extern "C" {
-    recording* recording_create(const std::string filename, 
-                                const std::vector<lsl::stream_info> streams, 
-                                const std::vector<std::string> watchfor, 
-                                const std::map<std::string, int> syncOptions, 
+    recording* recording_create(const char* filename, 
+                                const lsl::stream_info* streams, 
+                                const char** watchfor, 
+                                const std::map<std::string, int>* syncOptions,
                                 bool collect_offsets = true) 
     {
-        return new recording(filename, streams, watchfor, syncOptions, collect_offsets);
+        return new recording(
+            std::string(filename), 
+            std::vector<lsl::stream_info>(streams, streams + 1), 
+            std::vector<std::string>(watchfor, watchfor + 1), 
+            *syncOptions, 
+            collect_offsets
+        );
     }
 
     void recording_delete(recording* instance) 
